febf480e375505d3ba50637e167176b2be4c4b09
Ted Shin <tdyshin@gmail.com>
2017-08-23 18:26:07 -0400

    Update http to https

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var location_data = "http://ip-api.com/json";
+var location_data = "https://ip-api.com/json";
 
 function getLat(coords, commaInd) {
   var latitude = coords.substring(0, commaInd);
@@ -27,7 +27,7 @@ $.getJSON(location_data, function(json) {
   console.log(lat);
   console.log(lon);
 
-  var api = "http://api.openweathermap.org/data/2.5/weather?"
+  var api = "https://api.openweathermap.org/data/2.5/weather?"
   var appid = "appid=b25d08c92b96e29e064a0022300c81ec";
   lat = "lat=" + lat + "&";
   lon = "lon=" + lon + "&";
@@ -39,7 +39,7 @@ $.getJSON(location_data, function(json) {
     var tempInCelsius = Math.round(json.main.temp - 273.15);
     var tempInFah = Math.round(tempInCelsius * 9 / 5 + 32);
     var weatherType = json.weather[0].main;
-    var icon = "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
+    var icon = "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png";
 
     $("#temperature").html(tempInCelsius);
     $("#weather_type").html(weatherType);
